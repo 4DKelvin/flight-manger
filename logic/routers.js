cdf3c324176c97b84ed35bddd2e8479c1f25a693
CallmeLeeJJ <callmelijunjie@163.com>
2018-11-25 20:11:54 +0800

    third

--- logic/routers.js
+++ logic/routers.js
@@ -3,6 +3,7 @@ let router = express.Router();
 let Order = require('../model/order');
 let Customer = require('../model/customer');
 let Utils = require('../lib/utils');
+let Api = require('../lib/flight');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
@@ -35,4 +36,40 @@ router.get('/detail', async (req, res, next) => {
         endTime:endTime
     });
 });
+
+router.get('/refreshOrder', async (req, res, next) => {
+    if (req.query.orderNo) {
+        let order = await Api.orderDetail(req.query.orderNo);
+        if (order) {
+            Utils.renderJson(res, await Order.insertOrUpdate({
+                orderNo: order.detail.orderNo,
+                orderStatus: order.detail.status,
+                notice: order.other.tgqMsg,
+            }));
+        } else {
+            Utils.renderJsonError(res, "無結果");
+        }
+    } else {
+        Utils.renderJsonError(res, "參數錯誤");
+    }
+});
+
+router.get('/pay', async (req, res, next) => {
+    if (!isNaN(req.query.orderId) && req.query.orderAgent) {
+         //真實支付接口測試慎重
+                console.log(req.query.orderAgent+req.query.orderId)
+                try {
+                    let payRes = await Api.pay(req.query.orderId, req.query.orderAgent);
+                    let ticket = res.results[0];
+                    console.log(ticket); //支付成功，取得收據
+                    Utils.renderJson(res,payRes);
+                } catch (e) {
+                    console.log(e); //支付失敗，e=>原因
+                    Utils.renderJsonError(res, "無結果");
+                }
+    } else {
+        Utils.renderJsonError(res, "參數錯誤");
+    }
+});
+
 module.exports = router;
