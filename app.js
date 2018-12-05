5b119df6fca83cb3d0b3207dc21fbbb5b28e6afc
CallmeLeeJJ <callmelijunjie@163.com>
2018-12-05 18:05:27 +0800

    add_lock

--- app.js
+++ app.js
@@ -5,7 +5,51 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
 const sassMiddleware = require('node-sass-middleware');
+const session = require("express-session");
+const FileStore = require('session-file-store')(session);
 let app = express();
+var identityKey = 'skey';
+app.use(session({
+    name: identityKey,
+    secret: 'sessiontest',  // 用来对session id相关的cookie进行签名
+    store: new FileStore(),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
+    saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
+    resave: false,  // 是否每次都重新保存会话，建议false
+    cookie: {
+        maxAge: 1000 * 60 * 30 // 有效期，单位是毫秒。30分钟
+    }
+}));
+//登录拦截器
+/*app.all('/!*', function(req, res, next){
+    if (req.session.user) {
+        next();
+    }else {
+        var arr = req.oriq.split('/');// 解析用户请求的路径
+
+        for (var i = 0, length = arr.length; i < length; i++) {// 去除 GET 请求路径上携带的参数
+            arr[i] = arr[i].split('?')[0];
+        }
+        if (arr.length > 1 && arr[1] == '') {// 判断请求路径是否为根、登录、注册、登出，如果是不做拦截
+            next();
+        } else if (arr.length > 1 && arr[1] == 'loginPage' && (arr[2] == 'register' || arr[2] == 'login' || arr[2] == 'logout' || arr[2].indexOf('login') >= 0 )) {
+            next();
+        } else {  // 登录拦截
+            //req.session.originalUrl = req.originalUrl ? req.originalUrl : null;  // 记录用户原始请求路径
+            //req.flash('error', '请先登录');
+            //res.redirect('/loginPage');  // 将用户重定向到登录页面
+        }
+    }
+});*/
+
+app.use(function (req, res, next) {
+    var url = req.originalUrl;//获取浏览器中当前访问的nodejs路由地址；
+
+    if(url!='/loginPage'&& url!='/register' && !req.session.user && url!='/login'  ){ //通过判断控制用户登录后不能访问登录页面；
+        return res.redirect('/loginPage');//页面重定向；
+    }
+    next();
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
@@ -31,4 +75,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 module.exports = app;
--- logic/routers.js
+++ logic/routers.js
@@ -3,9 +3,11 @@ let router = express.Router();
 let Order = require('../model/order');
 let Utils = require('../lib/utils');
 let Api = require('../lib/flight');
+let Customer = require('../model/customer');
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
+
     var orders = await Order.search(req.query.start, req.query.end, req.query.orderId, req.query.tickerNo, req.query.name, req.query.page || 0);
 
     res.render('index', {
@@ -29,15 +31,30 @@ router.get('/', async (req, res, next) => {
 });
 
 router.get('/detail', async (req, res, next) => {
+
+    var flag = "0";
+    if(!req.session.user){
+        res.render('loginPage', { title: '登陆界面'});
+    }
+
     let orders = await new Promise((resolve, reject) => {
-        Order.query().where({groupId: req.query.orderNo}).lean().exec((err, orders) => {
+        Order.query().where({orderNo: req.query.orderNo}).lean().exec((err, orders) => {
             if (err) reject(err);
             else resolve(orders);
         })
     });
+    var lock = orders[0].lock;
+    if(lock){
+        flag = "1";
+        if(req.session.user.name==lock){
+            flag = "2";
+        }
+    }
+
+
 
     res.render('detail', {
-        title: '訂單詳情',
+        title: '訂單詳情', lock:flag,
         orders: orders.map((order) => {
             if (order.flightArrivalTime) {
                 order.flightArrivalTime = Utils.formatDateTime(order.flightArrivalTime);
@@ -126,5 +143,121 @@ router.get('/pay', async (req, res, next) => {
         Utils.renderJsonError(res, "參數錯誤");
     }
 });
+
+router.get('/loginPage', async (req, res, next) => {
+    res.render('loginPage', { title: '登陆界面'});
+});
+router.post('/login', async (req, res, next) =>{
+    if(!req.body.name||!req.body.password){
+        Utils.renderJsonError(res, "请输入账户和密码！");
+    }
+    var condition = {"name":req.body.name,"password":req.body.password}
+    var cust = await Customer.findByCon(condition);
+    if(cust.length==1){
+        var user={
+            name:req.body.name,
+            password:req.body.password
+        }
+        req.session.user=user;
+        //req.session.name = req.body.name; // 登录成功，设置 session
+        var orders = await Order.search(req.query.start, req.query.end, req.query.orderId, req.query.tickerNo, req.query.name, req.query.page || 0);
+        res.render('index', {
+            title: '訂單管理',
+            orders: orders.map((o) => {
+                if (o.flightDate) {
+                    o.date = Utils.formatDate(o.flightDate);
+                }
+                if (o.flightArrivalTime) {
+                    o.flightArrivalDateTime = Utils.formatDateTime(o.flightArrivalTime);
+                }
+                if (o.flightDepartureTime) {
+                    o.flightDepartureDateTime = Utils.formatDateTime(o.flightDepartureTime);
+                }
+                if (o.orderTotalPrice) {
+                    o.total = '¥ ' + Number(o.orderTotalPrice).toFixed(2);
+                }
+                return o;
+            })
+        });
+    }else{
+        Utils.renderJsonError(res, "不存在该用户！");
+    }
+});
+router.get('/register', async (req, res, next) => {
+    res.render('register', { title: '注册界面'});
+});
+router.post('/register', async (req, res, next) =>{
+    if(!req.body.name||!req.body.password||!req.body.sex||!req.body.certificatesNo){
+        Utils.renderJsonError(res, "请完善你的信息！");
+    }
+    var condition = {"name":req.body.name,"password":req.body.password}
+    var cust = await Customer.findByCon(condition);
+    console.log(cust.length);
+    if(cust.length==0){
+        var addJson = {
+            name: req.body.name,
+            password: req.body.password,
+            sex:  req.body.sex,
+            certificatesNo: req.body.certificatesNo,
+        };
+        await Customer.insert(addJson);
+        var user={
+            name:req.body.name,
+            password:req.body.password
+        }
+        req.session.user=user;
+        var orders = await Order.search(req.query.start, req.query.end, req.query.orderId, req.query.tickerNo, req.query.name, req.query.page || 0);
+        res.render('index', {
+            title: '訂單管理',
+            orders: orders.map((o) => {
+                if (o.flightDate) {
+                    o.date = Utils.formatDate(o.flightDate);
+                }
+                if (o.flightArrivalTime) {
+                    o.flightArrivalDateTime = Utils.formatDateTime(o.flightArrivalTime);
+                }
+                if (o.flightDepartureTime) {
+                    o.flightDepartureDateTime = Utils.formatDateTime(o.flightDepartureTime);
+                }
+                if (o.orderTotalPrice) {
+                    o.total = '¥ ' + Number(o.orderTotalPrice).toFixed(2);
+                }
+                return o;
+            })
+        });
+    }else{
+        Utils.renderJsonError(res, "已存在注册用户！");
+    }
+});
+
+router.get('/locked', async (req, res, next) => {
+    if (req.query.flag && req.query.orderNo) {
+        try {
+            var updateStr;
+            var flag;
+            if (req.query.flag=="0"){
+                updateStr = {$set:{"lock":req.session.user.name}};
+                flag = "2";
+            } else {
+                updateStr = {$set:{"lock":""}};
+                flag = "0";
+            }
+            var condition = {"orderNo":req.query.orderNo};
+            console.log(await Order.updateByCon(condition,updateStr));
+
+            var data = {"result":flag};
+            Utils.renderJson(res,flag);
+
+        } catch (e) {
+            Utils.renderJsonError(res, "支付失敗，原因：" + e);
+        }
+    } else {
+        Utils.renderJsonError(res, "參數錯誤");
+    }
+});
+
+
+
+
 
 module.exports = router;
