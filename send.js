38f5a1a01b10871fac2889833160b6ee7abf4b8e
Kelvin <4Kelvin@gmail.com>
2018-12-20 11:05:32 +0800

    fast commit

--- send.js
+++ send.js
@@ -5,9 +5,8 @@ let main = async () => {
     let orders = await Order.findByCon({
         groupId: "TAN1545213127447"
     });
-    for (let i = 0; i < orders.length; i++) {           
-	console.log(await Api.sendTicket(orders[i]));
-   	
+    for (let i = 0; i < orders.length; i++) {
+        console.log(await Api.sendTicket(orders[i]));
     }
 };
 
