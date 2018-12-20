ef6d0005db4a290869781302d898f1ca0f94f444
kelvin <kelvin@4dtechspace.com>
2018-12-20 11:04:28 +0800

    fast commit

--- send.js
+++ send.js
@@ -5,21 +5,9 @@ let main = async () => {
     let orders = await Order.findByCon({
         groupId: "TAN1545213127447"
     });
-
-    for (let i = 0; i < orders.length; i++) {
-        let local = orders[i];
-        let order = await Api.orderDetail(local.orderNo);
-        if (order) {
-            await Order.insertOrUpdate({
-                orderNo: order.detail.orderNo,
-                orderStatus: order.detail.status,
-                notice: order.other.tgqMsg,
-                passengerTicketNo: order.passengers[0].ticketNo
-            });
-            if (order.passengers[0].ticketNo) {
-                await Api.sendTicket(await Order.findById(order.detail.orderNo));
-            }
-        }
+    for (let i = 0; i < orders.length; i++) {           
+	console.log(await Api.sendTicket(orders[i]));
+   	
     }
 };
 
