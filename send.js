101f00480481124e605df1539fb0b539ef457d5d
Kelvin <4Kelvin@gmail.com>
2018-12-20 11:07:50 +0800

    fast commit

--- send.js
+++ send.js
@@ -5,6 +5,7 @@ let main = async () => {
     let orders = await Order.findByCon({
         groupId: "TAN1545213127447"
     });
+    console.log(orders);
     for (let i = 0; i < orders.length; i++) {
         console.log(orders[i]);
     }
