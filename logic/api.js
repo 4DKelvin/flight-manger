e2eada859d3b13141ea89668fcd1fe519bce8cb1
Kelvin <4Kelvin@gmail.com>
2018-12-27 16:07:22 +0800

    fast commit

--- logic/api.js
+++ logic/api.js
@@ -291,8 +291,8 @@ router.post('/ChangeSearch', async (req, res, next) => {
         let avResultList = [];
         for (let i = 0; i < dates.length; i++) {
             let local = os.find((o) => {
-                console.log(Utils.formatDate(o.flightDate), dates[i].departureDate);
-                if (Utils.formatDate(o.depAirportCode) === dates[i].depAirportCode) return o;
+                console.log(o.depAirportCode, dates[i].depAirportCode);
+                if (o.depAirportCode === dates[i].depAirportCode) return o;
             });
             let reasons = await Api.changeReasons(local.orderNo, dates[i].departureDate);
             if (!reasons[0].changeSearchResult.tgqReasons) {
