cf96858e94c18bb6007c4c89f1e499638fe3b2f7
Kelvin <4Kelvin@gmail.com>
2018-12-27 15:05:52 +0800

    fast commit

---
+++ model/ckey.js
@@ -0,0 +1,30 @@
+const keys = require('../db.js').change_key;
+const Utils = require("../lib/utils");
+
+module.exports = {
+    set: function (key, value) {
+        return new Promise(function (resolve, reject) {
+            let item = {
+                name: key || Utils.encodeBase64(new Date().getTime().toString()),
+                content: Utils.encodeBase64(JSON.stringify(value))
+            };
+            new keys(item).save(function (err, res) {
+                if (err) throw reject(err);
+                else resolve(item.name);
+            })
+        })
+    },
+    get: function (key) {
+        return new Promise(function (resolve, reject) {
+            keys.findOne({name: key}).lean().exec(function (err, data) {
+                try {
+                    if (err) reject(err);
+                    else resolve(JSON.parse(Utils.decodeBase64(data.content)));
+                } catch (e) {
+                    reject(e);
+                }
+            })
+        })
+    }
+
+};
